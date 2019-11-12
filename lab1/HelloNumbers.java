24266bb9ffb3df2795a4c2a1e73c5c2a9c7f61be
jawad <babarjawadk@gmail.com>
2019-11-12 14:52:48 +0100

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int i = 0;
+        while (i < 10) {
+            System.out.print(x + " ");
+            i = i + 1;
+            x = x + i;
+        }
+        System.out.println();
+    }
+}
