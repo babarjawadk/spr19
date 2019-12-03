141394a42c3fd70d6509ecf274bd17a92f7f86e8
jawad <babarjawadk@gmail.com>
2019-12-03 15:45:36 +0100

    hw03: core done

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,5 +1,6 @@
 package hw3.hash;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OomageTestUtility {
@@ -12,6 +13,20 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] spread;
+        int N = oomages.size();
+
+        spread = new int[M];
+        for (Oomage o : oomages) {
+            int bucketNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            spread[bucketNum] += 1;
+        }
+
+        for (Integer i : spread) {
+            if (i < N / 50 || i > N / 2) {
+                return false;
+            }
+        }
+        return true;
     }
 }
