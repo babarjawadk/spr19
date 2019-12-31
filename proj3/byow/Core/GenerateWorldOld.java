56a32ee36638dd303d05edbe1835e4b03c5697d0
jawad <babarjawadk@gmail.com>
2019-12-31 15:47:35 +0100

    proj3: wall <-> floor

--- proj3/byow/Core/GenerateWorldOld.java
+++ proj3/byow/Core/GenerateWorldOld.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class GenerateWorld {
+public class GenerateWorldOld {
 
     private class Room {
         private final int width;
@@ -17,19 +17,21 @@ public class GenerateWorld {
         private final Position topRight;
         private final Position bottomLeft;
         private final Position bottomRight;
+        private final Position center;
         private final boolean isInsideWorld;
 
-        private Room(Position pos, int w, int h) {
+        public Room(Position pos, int w, int h) {
             width = w;
             height = h;
             bottomLeft = pos;
             bottomRight = new Position(pos.x + w, pos.y);
             topLeft = new Position(pos.x, pos.y + h);
             topRight = new Position(pos.x + w, pos.y + w);
+            center = new Position((pos.x + pos.x + w) / 2, (pos.y + pos.y + h) / 2);
             isInsideWorld = calculateIsInsideWorld();
         }
 
-        private boolean isInsideWorld() {
+        public boolean isInsideWorld() {
             return isInsideWorld;
         }
 
@@ -43,18 +45,7 @@ public class GenerateWorld {
         }
 
         private boolean isInsideWorld(Position p) {
-            return p.x >= 0 && p.x < WIDTH && p.y >= 0 && p.y < HEIGHT;
-        }
-    }
-
-    private class Hallway {
-        private final int length;
-        private final boolean hasCorner;
-        private List<Room> connectedRooms = new ArrayList<>();
-
-        private Hallway(int l, boolean c) {
-            length = l;
-            hasCorner = c;
+            return p.x > 0 && p.x < WIDTH-1 && p.y > 0 && p.y < HEIGHT-1;
         }
     }
 
@@ -67,19 +58,13 @@ public class GenerateWorld {
 
     TETile[][] world;
 
-    GenerateWorld(int w, int h, long s) {
+    GenerateWorldOld(int w, int h, long s) {
         WIDTH = w;
         HEIGHT = h;
         SEED = s;
         RANDOM = new Random(SEED);
         world = createEmptyWorld(WIDTH, HEIGHT);
         totalNumberOfRooms = RANDOM.nextInt(25) + 11;
-
-
-        //Room room = createRandomRoom();
-        //addRoom(room);
-
-
         createRooms();
         addRooms();
     }
@@ -91,21 +76,17 @@ public class GenerateWorld {
     private void createRooms() {
         int numberOfRooms = 0;
         while (numberOfRooms < totalNumberOfRooms) {
-            Room room = createRandomRoom();
+            int w = RANDOM.nextInt(7) + 2;
+            int h = RANDOM.nextInt(7) + 2;
+            int xPos = RANDOM.nextInt(WIDTH);
+            int yPos = RANDOM.nextInt(HEIGHT);
+            Position pos = new Position(xPos, yPos);
+            Room room = new Room(pos, w, h);
             if (room.isInsideWorld() && doNotOverlap(rooms, room)) {
                 rooms.add(room);
                 numberOfRooms += 1;
             }
         }
-    }
-
-    private Room createRandomRoom() {
-        int w = RANDOM.nextInt(6) + 4;
-        int h = RANDOM.nextInt(6) + 4;
-        int xPos = RANDOM.nextInt(WIDTH);
-        int yPos = RANDOM.nextInt(HEIGHT);
-        Position pos = new Position(xPos, yPos);
-        return new Room(pos, w, h);
     }
 
     private static boolean doNotOverlap(List<Room> rooms, Room newRoom) {
@@ -124,12 +105,12 @@ public class GenerateWorld {
         Position r2 = room2.bottomRight;
 
         // If one rectangle is on left side of other
-        if (l1.x > r2.x || l2.x > r1.x) {
+        if (l1.x > r2.x+1 || l2.x > r1.x+1) {
             return false;
         }
 
         // If one rectangle is above other
-        if (l1.y < r2.y || l2.y < r1.y) {
+        if (l1.y+1 < r2.y || l2.y+1 < r1.y) {
             return false;
         }
 
@@ -149,19 +130,19 @@ public class GenerateWorld {
 
     private void makeFloor(Room room) {
         Position pos = room.bottomLeft;
-        for (int i = 0; i < room.width-2; i++) {
-            for (int j = 0; j < room.height-2; j++) {
-                world[pos.x + 1 + i][pos.y + 1 + j] = Tileset.FLOOR;
+        for (int i = 0; i < room.width; i++) {
+            for (int j = 0; j < room.height; j++) {
+                world[pos.x + i][pos.y + j] = Tileset.FLOOR;
             }
         }
     }
 
     private void makeWalls(Room room) {
         Position pos = room.bottomLeft;
-        makeHorizontalWall(new Position(pos.x, pos.y), room.width);
-        makeHorizontalWall(new Position(pos.x, pos.y + room.height - 1), room.width);
-        makeVerticalWall(new Position(pos.x, pos.y + 1), room.height-2);
-        makeVerticalWall(new Position(pos.x + room.width - 1, pos.y + 1), room.height-2);
+        makeHorizontalWall(new Position(pos.x - 1, pos.y - 1), room.width + 2);
+        makeHorizontalWall(new Position(pos.x - 1, pos.y + room.height), room.width + 2);
+        makeVerticalWall(new Position(pos.x - 1, pos.y - 1), room.height + 2);
+        makeVerticalWall(new Position(pos.x + room.width, pos.y - 1), room.height + 2);
     }
 
     private void makeVerticalWall(Position pos, int length) {
--- proj3/byow/Core/MapVisualTest.java
+++ proj3/byow/Core/MapVisualTest.java
@@ -6,9 +6,18 @@ import byow.TileEngine.TETile;
 public class MapVisualTest {
     private static final int WIDTH = 90;
     private static final int HEIGHT = 30;
-    private static final long SEED = 178;
+    private static final long SEED = 179;
+
+    public static void generateRandomRooms() {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        GenerateWorldOld gw = new GenerateWorldOld(WIDTH, HEIGHT, SEED);
+        TETile[][] world = gw.getWorld();
+        ter.renderFrame(world);
+    }
 
     public static void main(String[] args) {
+        //generateRandomRooms();
         TERenderer ter = new TERenderer();
         ter.initialize(WIDTH, HEIGHT);
         GenerateWorld gw = new GenerateWorld(WIDTH, HEIGHT, SEED);
